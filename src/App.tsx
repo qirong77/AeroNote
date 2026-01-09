f0ef22c3881808368019765e5a339c32d187928e
qirong77 <qirong7777@gmail.com>
2026-01-09 17:08:22 +0800

    feat: 功能开发 ✨

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import Editor from './components/Editor'
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
-        <h1>AeroNote</h1>
-      </header>
       <main className="app-content">
         <Editor />
       </main>
